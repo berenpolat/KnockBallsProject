6ffb92f2f2659a3b8a8f7a695f80b648073a85d5
berenelcn <polatberen24@gmail.com>
2024-04-10 20:01:24 +0300

    StartScreen Components Setted

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    #region Panels
 
+    [SerializeField] private GameObject startPanel;
+    
+
+    #endregion
+    
+    
     #region Unity Methods
     // Start is called before the first frame update
     void Start()
@@ -20,9 +27,20 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
+    private void StartTheGame()
+    {
+        startPanel.SetActive(false);
+    }
+    
+    
+    
 
     #region Button Controls
 
+    public void OnClickedTapToPlayButton()
+    {
+        StartTheGame();
+    }
 
     #endregion
 }
