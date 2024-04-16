0644ed098ab6a09bb519eec14451133e3b7be006
Gizoskos <gizemgultoprak2004@gmail.com>
2024-04-16 17:57:09 +0300

    Automated Ending Circle
    Update e topun bittiği bilgisi  gitmeli.

--- Assets/Scripts/CircleTransition.cs
+++ Assets/Scripts/CircleTransition.cs
@@ -8,6 +8,7 @@ public class CircleTransition : MonoBehaviour
 {
     private Canvas canvas;
     public Image blackScreen;
+    private static readonly int Radius = Shader.PropertyToID("Radius");
 
     private void Awake()
     {
@@ -17,6 +18,29 @@ public class CircleTransition : MonoBehaviour
     private void Start()
     {
         DrawBlackScreen();
+    }
+
+    private void Update()
+    {
+        //When we run out of balls the coroutine starts
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OpenBlackScreen();
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            CloseBlackScreen();
+        }
+    }
+
+    public void OpenBlackScreen()
+    {
+        StartCoroutine(Transition(2, 0, 1));
+    }
+
+    public void CloseBlackScreen()
+    {
+        StartCoroutine(Transition(2, 1, 0));
     }
 
     private void DrawBlackScreen()
@@ -37,6 +61,18 @@ public class CircleTransition : MonoBehaviour
         }
 
         blackScreen.rectTransform.sizeDelta = new Vector2(squareValue, squareValue);
+    }
 
+    private IEnumerator Transition(float duration, float beginRadius, float endRadius)
+    {
+        var time = 0f;
+        while (time <= duration)
+        {
+            time += Time.deltaTime;
+            var t = time / duration;
+            var radius = Mathf.Lerp(beginRadius, endRadius, t);
+            blackScreen.material.SetFloat(Radius, radius);
+            yield return null;
+        }
     }
 }
